895cf4b7ae437f40f51fa46857e059e9851f5c5b
bala-amypo <bala@amypo.in>
2025-12-17 09:18:13 +0000

    User commit

--- src/main/java/com/example/demo/services/StudentService.java
+++ src/main/java/com/example/demo/services/StudentService.java
@@ -1,36 +1,14 @@
 package com.example.demo.services;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
-import org.springframework.stereotype.Service;
+import com.example.demo.entity.Studentity;
 
-import com.example.demo.entity.Student;
-
-@Service
-public class StudentServiceImpl implements StudentService {
-
-    private final Map<Integer, Student> store = new HashMap<>();
-    private int counter = 1;
-
-    @Override
-    public Student insertStudent(Student st) {
-        st.setId(counter++);
-        store.put(st.getId(), st);
-        return st;
-    }
-
-    @Override
-    public List<Student> getAllStudents() {
-        return new ArrayList<>(store.values());
-    }
-
-    @Override
-    public Optional<Student> getOneStudent(int id) {
-        return Optional.ofNullable(store.get(id));
-    }
-
-    @Override
-    public void deleteStudent(int id) {
-        store.remove(id);
-    }
+public interface Studentservice {
+    
+    Studentity insertStudent(Studentity st);
+    List<Studentity> getAllStudents();
+    Optional<Studentity> getOneStudent(int id);
+    void deleteStudent(int id);
 }
--- src/main/java/com/example/demo/services/StudentServiceImpl.java
+++ src/main/java/com/example/demo/services/StudentServiceImpl.java
@@ -1,5 +1,5 @@
 
-package com.example.demo.service;
+package com.example.demo.services;
 
 import java.util.*;
 import org.springframework.stereotype.Service;
